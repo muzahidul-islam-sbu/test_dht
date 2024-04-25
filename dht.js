b93064654ea23260327932dce0393191897a15fd
Justin Chen <jchen9182@gmail.com>
2024-04-24 22:40:25 -0400

    ugh

--- dht.js
+++ dht.js
@@ -19,7 +19,8 @@ async function createNode() {
                 kBucketSize: 20,
                 allowQueryWithZeroPeers: true,
                 querySelfInterval: 10,
-                peerInfoMapper: removePublicAddressesMapper
+                peerInfoMapper: removePublicAddressesMapper,
+                protocol: '/ipfs/lan/kad/1.0.0'
             })
         },
         peerDiscovery: [
@@ -38,22 +39,23 @@ async function createNode() {
 }
 
 import readline from 'readline'
-async function main() {
+import { getPackedSettings } from 'http2'
+async function init() {
     const node = await createNode();
     const discoveredPeerIds = []
     // Log peer discovery events
     node.addEventListener('peer:discovery', (connection) => {
-        console.log('Discovered peer:' ,connection.detail.id, connection.detail.multiaddrs);
+        console.log('Discovered peer:', connection.detail.id, connection.detail.multiaddrs);
         discoveredPeerIds.push(connection.detail.id)
     });
-    
+
     // Log peer connection events
     node.addEventListener('peer:connect', async (connection) => {
         console.log('Connected to peer:', connection, connection.detail, connection.detail.multiaddrs);
-        
+
         let retrievedValue = node.services.dht.findPeer(connection.detail);
         for await (const queryEvent of retrievedValue) {
-            console.log('connect' , queryEvent)
+            console.log('dht:connect', queryEvent)
         }
 
         await node.dial(connection.detail)
@@ -62,17 +64,21 @@ async function main() {
     node.addEventListener(EventTypes.VALUE, () => {
         console.log('found')
     })
-    
+
     console.log('PeerID', node.peerId.toString());
     node.getMultiaddrs().forEach((addr) => {
         console.log(addr.toString());
     });
-    
+
     // Start the DHT
-    await node.services.dht.setMode('server');
+    node.services.dht.log.enabled = true;
     await node.services.dht.start();
+    await node.services.dht.setMode('server');
     await node.start();
-    
+
+    return node;
+}
+async function cli(node) {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
@@ -98,8 +104,22 @@ async function main() {
                     console.error('Usage: get <key>');
                     break;
                 }
-                await node.services.dht.refreshRoutingTable()
+                await node.services.dht.refreshRoutingTable();
                 await getValue(node, args[0]);
+                break;
+            case 'get_mode':
+                if (args.length !== 0) {
+                    console.error('Usage: get_mode');
+                    break;
+                }
+                console.log(node.services.dht.getMode());
+                break;
+            case 'set_mode':
+                if (args.length !== 1) {
+                    console.error('Usage: set_mode <client/server>');
+                    break;
+                }
+                await node.services.dht.setMode(args[0]);
                 break;
             default:
                 console.error('Unknown command:', command);
@@ -127,24 +147,46 @@ async function main() {
 async function putKeyValue(node, key, value) {
     const keyUint8Array = new TextEncoder('utf8').encode(key);
     const valueUint8Array = new TextEncoder('utf8').encode(value);
-    let retrievedValue = node.services.dht.put(keyUint8Array, valueUint8Array ,{useNetwork: true, useCache: false});
-    for await (const queryEvent of retrievedValue) {
-        console.log('put' , queryEvent)
+    try {
+        console.log("trying put keyvalue...");
+        let retrievedValue = node.services.dht.put(keyUint8Array, valueUint8Array, { useNetwork: true, useCache: false });
+        for await (const queryEvent of retrievedValue) {
+            console.log('dht:put', queryEvent)
+        }
+    } catch (err) {
+        throw err;
     }
-    console.log(`Key "${key}" with value "${value}" successfully added to the DHT.`);
 }
 
 async function getValue(node, key) {
-    const keyUint8Array =  new TextEncoder('utf8').encode(key);
-    let retrievedValue
-    retrievedValue = node.peerRouting.getClosestPeers(keyUint8Array)
+    console.log("trying get value...");
+    const keyUint8Array = new TextEncoder('utf8').encode(key);
+    let retrievedValue;
+    retrievedValue = node.services.dht.getClosestPeers(keyUint8Array)
     for await (const queryEvent of retrievedValue) {
-        console.log('peerRouting' , queryEvent)
+        console.log('dht:peerRouting', queryEvent)
     }
-    retrievedValue = node.services.dht.get(keyUint8Array,  {useNetwork: true, useCache: false});
+    retrievedValue = node.services.dht.get(keyUint8Array, { useNetwork: true, useCache: false });
     for await (const queryEvent of retrievedValue) {
-        console.log('get' , queryEvent)
+        console.log('dht:get', queryEvent)
     }
 }
 
-main();
+// import { createHash } from 'crypto';
+// const hash = createHash('sha256').update('test').digest('hex');
+// const k = '/pk/' + hash;
+
+const k = '/test/test';
+const v = 'a';
+let node = await init();
+await putKeyValue(node, k, v);
+await getValue(node, k);
+await node.services.dht.setMode('server');
+
+let node2 = await init();
+await new Promise(r => setTimeout(r, 2000));
+console.log('node2 stuff');
+console.log(node2.services.dht.getMode());
+await getValue(node2, k);
+
+// await cli(node);
